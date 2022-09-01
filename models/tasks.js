343208902f21fb023889e19f75044f9801d3909f
Emmanuel Onyo <emmanuelonyo34@gmail.com>
2022-09-01 09:56:17 +0100

    FULL TO DO APP

--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    completed:Boolean
+    name:{
+        type:String,
+        required:[true, "Name Value is Required"],
+        trim: true,
+        maxlength:[20, "name cannot be more than 20"]
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
